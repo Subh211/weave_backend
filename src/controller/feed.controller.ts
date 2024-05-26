54fe07e3d7b8e74f54be4520f7dc11f734fc9db5
Subh211 <dassubhadeep211@gmail.com>
2024-05-26 21:59:07 +0530

    feed added with all users

--- src/controller/feed.controller.ts
+++ src/controller/feed.controller.ts
@@ -7,11 +7,9 @@ import User from "../Models/user.schema";
 import AppError from "../Utils/appError";
 
 
-
-
 const feed = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        //posts of followers
+        //////// get all posts of my followings
 
         //ger userid from jwtAuth middleware
         const userId = req.user?.id;
@@ -30,6 +28,9 @@ const feed = async (req: Request, res: Response, next: NextFunction) => {
             { $unwind: "$following" },
             { $project: { _id: 0, friendId: "$following.friendId" } }
         ]);
+
+        //newResult is created for sorting purpose between my followings and rest of the users
+        const newResult = result.map(item => item.friendId);
 
         //get only the frirndId's by mapping over result array
         const friendIds = result.map(doc => doc.friendId);
@@ -91,7 +92,7 @@ const feed = async (req: Request, res: Response, next: NextFunction) => {
                                 picture_secure_url: userPostDetails[j]?.image?.secure_url
                             };
 
-                            //now push each posts to the totaoPost array
+                            //now push each posts to the totalPost array
                             totalPosts.push(eachPosts);
                         
                     }
@@ -99,44 +100,117 @@ const feed = async (req: Request, res: Response, next: NextFunction) => {
             }
         }
 
-        //now sort the totalPost array by filling the unlikedPosts with those posts only which are not liked by user 
-        let unlikedPosts: PostDetail[] = [];
+        //////// get all posts of my followings
 
-            //loop through totalpost array
-            for (let a = 0; a < totalPosts.length; a++) { // Changed `<=` to `<`
-                let likedByUser = totalPosts[a].likes;
 
-                let isLiked = false;
-                if (likedByUser) {
-                    for (let b = 0; b < likedByUser.length; b++) { // Changed `<=` to `<`
-                        if (likedByUser[b].userId === userId) {
-                            isLiked = true;
-                            break; // Exit the loop if the post is liked by the user
+
+
+                //////// get all posts for the rest of the users
+
+                // to get rest of the user posts other than my followings fetch all users and project only the _id field
+                const users = await User.find({}, { _id: 1 });
+                
+                // Extract the _id values into an array
+                const allUser = users.map(user => user._id);
+
+                // Filter allUser to exclude elements present in newResult
+                const restUsersThanMyFollowings = allUser.filter(user => 
+                    !newResult.some(result => result.equals(user))
+                );
+                        
+                //run a for loop for every element of the allUser array
+                for (let i = 0; i < restUsersThanMyFollowings.length; i++) {
+
+                //define the userIdObject as allUser[i]
+                const userIdObject = restUsersThanMyFollowings[i];
+
+                //find the user from User
+                const user = await User.findOne({ _id: userIdObject });
+
+                //get his displayname
+                const userName = user?.displayName;
+
+                //get the users all posts from Post
+                const post = await Post.findOne({ userId: userIdObject });
+
+                //first check if he has any posts or not
+                const userPostDetails = post?.posts;
+
+                //if he has any posts
+                if (userPostDetails) {
+
+                    //now loop through the every post of that user
+                    for (let j = 0; j < userPostDetails.length; j++) {
+
+                        //define the currentPostId
+                        let currentPostId = userPostDetails[j]._id;
+
+                        //if current PostId found---
+                        if (currentPostId) {
+
+                                //fill the details 
+                                const eachPosts: PostDetail = {
+                                    name: userName,
+                                    caption: userPostDetails[j].caption,
+                                    likes: userPostDetails[j].likes,
+                                    comments: userPostDetails[j].comments?.map(comment => ({
+                                        comment: comment.comment || "",
+                                        userId: comment.userId || "",
+                                        userName: comment.userName || ""
+                                    })),
+                                    picture_public_id: userPostDetails[j]?.image?.public_id,
+                                    picture_secure_url: userPostDetails[j]?.image?.secure_url
+                                };
+
+                                //now push each posts to the totalPost array
+                                totalPosts.push(eachPosts);
+                            
                         }
                     }
                 }
+            }
 
-                // Add to unlikedPosts if the post is not liked by the user
-                if (!isLiked) {
-                    unlikedPosts.push(totalPosts[a]);
+
+            //////// get all posts for the rest of the users
+
+
+
+        //now sort the totalPost array by filling the unlikedPosts with those posts only which are not liked by user 
+        let unlikedPosts: PostDetail[] = [];
+
+        //loop through totalpost array
+        for (let a = 0; a < totalPosts.length; a++) { // Changed `<=` to `<`
+            let likedByUser = totalPosts[a].likes;
+
+            let isLiked = false;
+            if (likedByUser) {
+                for (let b = 0; b < likedByUser.length; b++) { // Changed `<=` to `<`
+                    if (likedByUser[b].userId === userId) {
+                        isLiked = true;
+                        break; // Exit the loop if the post is liked by the user
+                    }
                 }
             }
 
-        res.status(200).json({
-            success: true,
-            message: "feed",
-            data: unlikedPosts,
-        });
-
-    } catch (error: any) {
-        //error handling
-        console.error("Error fetching feed:", error);
-        next(new AppError("Internal server error", 500));
-    }
-};
+            // Add to unlikedPosts if the post is not liked by the user
+            if (!isLiked) {
+                unlikedPosts.push(totalPosts[a]);
+            }
+        }
 
 
+            res.status(200).json({
+                success: true,
+                message: "feed",
+                data: unlikedPosts,
+            });
 
+        } catch (error: any) {
+            //error handling
+            console.error("Error fetching feed:", error);
+            next(new AppError("Internal server error", 500));
+        }
+    };
 
 
 
